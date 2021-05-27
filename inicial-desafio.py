7ea1741048264f96971b0a7dad40671db8f8fb38
Mateus Xavier <51239552+sauMte@users.noreply.github.com>
2021-05-27 20:13:54 -0300

    Create inicial-desafio.py

---
+++ inicial-desafio.py
@@ -0,0 +1,6 @@
+animesNames = ["Fullmetal Alchemist: Brotherhood", "One Piece", "Naruto", "Cowboy Bebop", "Neon Genesis Evangelion", "Hunter x Hunter (2011)", "Vinland Saga", "Tengen Toppa Gurren Lagann", "Yakusoku no Neverland", "Death Note"]
+
+animesScores = [9.19, 8.52, 7.91, 8.78, 8.32, 9.1, 8.71, 8.66, 8.65, 8.63]
+
+for i in range(10):
+	print(f"O anime {animesNames[i]} tem nota {animesScores[i]}.")
